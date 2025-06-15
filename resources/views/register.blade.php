f5f401b4de5aa7b318af2a069d199a224c382982
= <codethencode@gmail.com>
2025-06-15 17:22:46 +0200

    Corrige button gogole

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -10,7 +10,8 @@ Ce service est réservé aux avocats et cabinets d'avocats.<br>Afin de calculer 
                             Nouveau membre ?<br><br>
 
 
-                            <a href="{{ route('auth.google.redirect') }}" class="w-full dark:text-gray-800 flex items-center justify-center px-4 py-3 mb-4 bg-white border border-gray-300 rounded shadow hover:bg-gray-50">
+                            <a href="{{ route('auth.google.redirect') }}"
+                            class="w-full dark:text-gray-800 flex items-center justify-center px-4 py-3 mb-4 border border-gray-300 rounded shadow hover:bg-gray-50">
     <img src="/images/google-com.svg" class="w-5 h-5 mr-2" alt="Google logo">
     <span>Continuer avec Google</span>
 </a>
@@ -64,7 +65,7 @@ Ce service est réservé aux avocats et cabinets d'avocats.<br>Afin de calculer 
 
                             Dejà membre ?<br><br>
 
-                            <a href="{{ route('auth.google.redirect') }}" class="w-full dark:text-gray-800 flex items-center justify-center px-4 py-3 mb-4 bg-white border border-gray-300 rounded shadow hover:bg-gray-50">
+                            <a href="{{ route('auth.google.redirect') }}" class="w-full dark:text-gray-800 flex items-center justify-center px-4 py-3 mb-4 border border-gray-300 rounded shadow hover:bg-gray-50">
     <img src="/images/google-com.svg" class="w-5 h-5 mr-2" alt="Google logo">
     <span>Se connecter avec Google</span>
 </a>
